4e0c102b1e4eef14d3ed97178d1215e4417bcac4
Jedle <jedlicka@kky.zcu.cz>
2022-04-28 10:25:34 +0200

    repaired

--- lib/trc_writer.py
+++ lib/trc_writer.py
@@ -8,24 +8,23 @@ def write_trc(trc_file, trc_data, trajectory, channels, startFrameNumber=1):
     line = ''
     for item in trc_data.MarkerNames:
         line += item + '\t'
-    line = line[:-2] + '\n'
+    line = line[:-1] + '\n'
     content.append(line)
     line = ''
     for item in trc_data.Channels:
         line += item + '\t'
-    line = line[:-2] + '\n'
+    line = line[:-1] + '\n'
     content.append(line)
 
     for i in range(np.size(trajectory, 0)):
-        line = str(i+startFrameNumber) + '\t' + '' +'\t'  # frame number, time(no input)
-        for j in range(np.size(trajectory, 1)):
-            for e, ch in enumerate(trc_data.Channels[2:]):
-                column = ([ex for ex, x in enumerate(channels) if x[1] == ch])
-                if column:
-                    print(type(column))
-                    line += str(trajectory[i, column[0]]) + '\t'
-                else:
-                    line += '\t'
-
-            print(line)
-            return
+        line = str(i+startFrameNumber) + '\t' + str(float(i)/float(trc_data.CameraRate)) +'\t'  # frame number, time
+        for e, ch in enumerate(trc_data.Channels[2:]):
+            column = ([ex for ex, x in enumerate(channels) if x[1] == ch])
+            if column:
+                line += str(trajectory[i, column[0]]) + '\t'
+            else:
+                line += '\t'
+        line = line[:-2] + '\n'
+        content.append(line)
+    with open(trc_file, 'w') as f:
+        f.writelines(content)
