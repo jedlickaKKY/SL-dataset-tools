e1a9b1fb170b3e027000a0fb67bfd9b1fc34bc3c
Jedle <jedlicka@kky.zcu.cz>
2022-04-29 13:58:52 +0200

    cleaned

--- lib/sign_dictionary.py
+++ lib/sign_dictionary.py
@@ -1,8 +1,5 @@
 import pickle
 
-# class dict_item:
-#     def __init__(self, mocap_source_file, video_reference_file, sl_annotator, video_time_stamp, mocap_time_stamp, mocap_cleaner, signer, annot_default, annot_left_hand, annot_right_hand, codified_meaning):
-
 def load_dictionary(path):
     with open(path, 'rb') as f:
         loaded = pickle.load(f)
